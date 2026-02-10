54c083e270759ed166b9091effb832f6caf402cb
Duncan Likely (Student) <duncan.likely.2021@uni.strath.ac.uk>
2026-02-10 17:27:50 +0000

    added ccs project

---
+++ assignment_1_ccs/data.h
@@ -0,0 +1,15 @@
+/*
+ * data.h
+ *
+ *  Created on: 10 Feb 2026
+ *      Author: Duncan Likely
+ */
+
+#ifndef DATA_H_
+#define DATA_H_
+
+#define X1[9] = {2, 0, 2, 1, 1, 2, 9, 8, 5}
+#define X2[9] = {2, 0, 2, 1, 2, 9, 6, 6, 3}
+
+
+#endif /* DATA_H_ */
---
+++ assignment_1_ccs/main.c
@@ -0,0 +1,13 @@
+#include "data.h"
+
+
+#define SIG_LEN 900
+#define SIG_PERIOD
+/**
+ * main.c
+ */
+int main(void)
+{
+    for (i)
+	return 0;
+}
