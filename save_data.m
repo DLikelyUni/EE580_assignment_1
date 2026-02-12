11914ef92ebca3c6958ab0833431ddcd2532e917
Duncan Likely <duncan.likely.2021@uni.strath.ac.uk>
2026-02-12 15:27:45 +0000

    altered save data code

--- save_data.m
+++ save_data.m
@@ -1,24 +1,24 @@
-fid = fopen('data.h','w');
+fid = fopen('.\assignment_1_ccs\data.h','w');
 %% FIR FILTER
 %% B
-fprintf(fid,['#define N_FIR_B %d' char([13 10])], length(b_fir));
+fprintf(fid,['#define N_FIR_B %d' char([13 10])], length(Hd2));
 fwrite(fid,char([13 10]),'uchar');
 
-fwrite(fid,'float b_fir[] = { ','uchar');
-for ct = 1:length(b_fir)-1
-    fprintf(fid,'%.7ff, ', single(b_fir(ct)));
+fwrite(fid,'float Hd2[] = { ','uchar');
+for ct = 1:length(Hd2)-1
+    fprintf(fid,'%.7ff, ', single(Hd2(ct)));
 end
-fprintf(fid,'%.7ff', single(b_fir(end)));
+fprintf(fid,'%.7ff', single(Hd2(end)));
 fwrite(fid,[' };' char([13 10])],'uchar');
 fwrite(fid,char([13 10]),'uchar');
 %% A
-fprintf(fid,['#define N_FIR_A %d' char([13 10])], length(a_fir));
+fprintf(fid,['#define N_FIR_A %d' char([13 10])], length(Hd1));
 fwrite(fid,char([13 10]),'uchar');
-fwrite(fid,'float a_fir[] = { ','uchar');
-for ct = 1:length(a_fir)-1
-    fprintf(fid,'%.7ff, ', single(a_fir(ct)));
+fwrite(fid,'float Hd1[] = { ','uchar');
+for ct = 1:length(Hd1)-1
+    fprintf(fid,'%.7ff, ', single(Hd1(ct)));
 end
-fprintf(fid,'%.7ff', single(a_fir(end)));
+fprintf(fid,'%.7ff', single(Hd1(end)));
 fwrite(fid,[' };' char([13 10])],'uchar');
 fwrite(fid,char([13 10]),'uchar');
 
