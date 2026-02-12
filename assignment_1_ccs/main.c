37bf6ac6a8555f65276b11542b7263a2da33c368
Duncan Likely <duncan.likely.2021@uni.strath.ac.uk>
2026-02-12 15:48:25 +0000

    fixed array indexing

--- assignment_1_ccs/main.c
+++ assignment_1_ccs/main.c
@@ -17,16 +17,16 @@ int main(void)
   float mean_x2 = 0;
 
   for (i = 0; i < SIG_PERIOD; i++){
-    mean_x1 += (float) X1(i);
-    mean_x2 += (float) X2(i);
+    mean_x1 += (float) u_1[i];
+    mean_x2 += (float) u_2[i];
   }
   mean_x1 = (float) mean_x1 / SIG_PERIOD;
   mean_x2 = (float) mean_x2 / SIG_PERIOD;
 
     for (i = 0; i < SIG_LEN; i++){
       stu_num_element = i % SIG_PERIOD;
-      x1(i) = (float) X1(stu_num_element) - mean_x1;
-      x2(i) = (float) X2(stu_num_element) - mean_x2;
+      x1[i] = (float) u_1[stu_num_element] - mean_x1;
+      x2[i] = (float) u_2[stu_num_element] - mean_x2;
     }
 	return 0;
 }
