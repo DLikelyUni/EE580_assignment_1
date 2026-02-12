2f43e3a06c05b4b85d346af87a5141afc7172243
Lewis Carty (Student) <lewis.carty.2021@uni.strath.ac.uk>
2026-02-12 15:11:31 +0000

    Task one files

--- EE580_assignment_1.m
+++ EE580_assignment_1.m
@@ -3,14 +3,14 @@
 % Duncan Likely
 
 clear;
-stunum_l=[2,0,2,1,1,2,9,8,5];
-stunum_d=[2,0,2,1,2,9,6,6,3];
-x1=stunum_l;
-x2=stunum_d;
+u_1=[2,0,2,1,1,2,9,8,5];
+u_2=[2,0,2,1,2,9,6,6,3];
+x1=u_1;
+x2=u_2;
 
 for i=1:99 %Generate signal with length N=900 samples
-    x1=[x1,stunum_l];
-    x2=[x2,stunum_d];
+    x1=[x1,u_1];
+    x2=[x2,u_2];
 end
 x1=x1-mean(x1);%zero mean signal
 x2=x2-mean(x2);%zero mean signal
@@ -24,9 +24,29 @@ X2=fft(x2,1024);%1024 point Fast Fourier transform of x2
 load("Hd1.mat");
 load("Hd2.mat");
 
+%%
+Hd1_hold=Hd1;
+Hd2_hold=Hd2;
 Hd1=single(Hd1.Numerator);
 Hd2=single(Hd2.Numerator);
+%%
+figure(1)
+zplane(Hd1_hold);
+title('h_1[n]: Pole Zero - Pre-Quantization')
 
+figure(2)
+zplane(Hd1);
+title('h_1[n]: Pole Zero - Quantized')
+
+figure(3)
+zplane(Hd2_hold);
+title('h_2[n]: Pole Zero - Pre-Quantization')
+
+figure(4)
+zplane(Hd2);
+title('h_2[n]: Pole Zero - Quantized')
+
+%%
 y1=filter(Hd1,1,x1);
 Y1=fft(y1,1024);%1024 point Fast Fourier transform of y1
 
@@ -42,45 +62,45 @@ figure(1)
 tiledlayout(2,1)
 nexttile
 plot(t(1:27)*1000,x1(1:27));
-title('x1: Lewis')
+title('x_1[n]')
 xlabel('t(ms)')
 ylabel('amplitude')
 nexttile
 plot(t(1:27)*1000,x2(1:27));
-title('x2: Duncan')
+title('x_2[n]')
 xlabel('t(ms)')
 ylabel('amplitude')
 
 %% fft of original signals
-% figure(2)
-% tiledlayout(2,1)
-% nexttile
-% plot(fs/L*(-L/2:L/2-1),abs(fftshift(X1)));
-% % xlim([0 30]);
-% title('|fft X1|')
-% xlabel('f (Hz)')
-% ylabel('amplitude')
-% nexttile
-% plot(fs/L*(-L/2:L/2-1),abs(fftshift(X2)));
-% % xlim([0 30]);
-% title('|fft X2|')
-% xlabel('f(Hz)')
-% ylabel('amplitude')
+figure(2)
+tiledlayout(2,1)
+nexttile
+plot(fs/L*(-L/2:L/2-1),abs(fftshift(X1)));
+% xlim([0 30]);
+title('|fft X1|')
+xlabel('f (Hz)')
+ylabel('amplitude')
+nexttile
+plot(fs/L*(-L/2:L/2-1),abs(fftshift(X2)));
+% xlim([0 30]);
+title('|fft X2|')
+xlabel('f(Hz)')
+ylabel('amplitude')
 
 %% Bandstop filtered signal x1[n]
 
-figure('Name','x1 vs y1')
+figure('Name','x_1 vs y_1')
 tiledlayout(2,1)
 nexttile
 plot(fs/L*(-L/2:L/2-1),abs(fftshift(X1)));
 % ylim([0 1600]);
-title('|fft X1|')
+title('|fft X_1[n]|')
 xlabel('f (Hz)')
 ylabel('amplitude')
 nexttile
 plot(fs/L*(-L/2:L/2-1),abs(fftshift(Y1)));
 % ylim([0 1600]);
-title('band stop')
+title('|fft Y_1[n]|')
 xlabel('f(Hz)')
 ylabel('amplitude')
 
@@ -91,8 +111,8 @@ hold on;
 plot(fs/L*(-L/2:L/2-1),abs(fftshift(Y1)),'--r');
 xlabel('f(Hz)')
 ylabel('amplitude')
-legend('|fft X1|','|fft bandstop X1|')
-title('X1, Bandstop, fd=705Hz')
+legend('|fft X_1|','|fft Y_1|')
+title('X1 vs Y1, fd=705Hz')
 
 %% Bandstop filtered signal x1[n]
 figure('Name','x2 vs y2')
@@ -100,15 +120,15 @@ tiledlayout(2,1)
 nexttile
 plot(fs/L*(-L/2:L/2-1),abs(fftshift(X2)));
 % ylim([0 1600]);
-title('|fft X2|')
+title('|fft X_2|')
 xlabel('f (Hz)')
 ylabel('dB')
 nexttile
 plot(fs/L*(-L/2:L/2-1),abs(fftshift(Y2)));
 % ylim([0 1600]);
-title('band stop')
+title('|fft Y_2|')
 xlabel('f(Hz)')
-ylabel('dB')
+ylabel('amplitude')
 
 %overlay x2 vs bandstop
 figure('Name','x2 vs y2:overlay')
@@ -117,53 +137,55 @@ hold on;
 plot(fs/L*(-L/2:L/2-1),abs(fftshift(Y2)),'--r');
 xlabel('f(Hz)')
 ylabel('amplitude')
-legend('|fft X2|','|fft bandstop X2|')
-title('X2, Bandstop, fd=2810Hz')
+legend('|fft X_2|','|fft Y_2|')
+title('X_2 vs Y_2, fd=2810Hz')
 
 %% time series plots of bandpassed signals
 figure('Name','time series y1, y2')
-tiledlayout(2,2)
-nexttile
-plot(t900(401:427)*1000,x1(402:428));
-title('x1: Lewis')
-xlabel('t(ms)')
-ylabel('amplitude')
+tiledlayout(2,1)
+% nexttile
+% plot(t900(401:427)*1000,x1(402:428));
+% title('x_1')
+% xlabel('t(ms)')
+% ylabel('amplitude')
 nexttile
 plot(t900(401:427)*1000,x2(402:428));
-title('x2: Duncan')
+title('x_2')
 xlabel('t(ms)')
 ylabel('amplitude')
-nexttile
-plot(t900(401:427)*1000,y1(401:427));
-% ylim([0 1600]);
-title('y1')
-xlabel('t(ms)')
-ylabel('amplitude')
+% nexttile
+% plot(t900(401:427)*1000,y1(401:427));
+% % ylim([0 1600]);
+% title('y_1')
+% xlabel('t(ms)')
+% ylabel('amplitude')
 nexttile
 plot(t900(401:427)*1000,y2(401:427));
 % ylim([0 1600]);
-title('y2')
+title('y_2')
 xlabel('t(m)')
 ylabel('amplitude')
 
 %% time series plots of bandpassed signals, overlay
 
 figure('Name','time series overlays y1, y2')
-tiledlayout(1,2)
-nexttile
-plot(t900(401:427)*1000,x1(402:428));
-hold on;
-plot(t(401:427)*1000,y1(401:427),'--r');
-grid on;
-title('x1,y1')
-xlabel('t(m)')
-ylabel('amplitude')
-nexttile
+% tiledlayout(1,2)
+% nexttile
+% plot(t900(401:427)*1000,x1(402:428));
+% hold on;
+% plot(t(401:427)*1000,y1(401:427),'--r');
+% grid on;
+% title('x_1 vs y_1')
+% legend('x_1[n]','y_1[n]')
+% xlabel('t(ms)')
+% ylabel('amplitude')
+% nexttile
 plot(t900(401:427)*1000,x2(402:428));
 hold on;
 plot(t900(401:427)*1000,y2(401:427),'--r');
 grid on;
-title('x2,y2')
-xlabel('t(m)')
+title('x_2[n] vs y_2[n]')
+legend('x_2[n]','y_2[n]')
+xlabel('t(ms)')
 ylabel('amplitude')
 %% plot in dBfigure('Name','x2 vs y2, dB')
